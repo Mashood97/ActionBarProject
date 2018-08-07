72e45faeda49e05900d26f8d5494f3fdacc77388
Mashood97 <40804440+Mashood97@users.noreply.github.com>
2018-08-07 11:30:23 +0500

    initial commit

---
+++ app/src/main/java/com/example/okcomputers/actionbarproject/Order_Activity.java
@@ -0,0 +1,13 @@
+package com.example.okcomputers.actionbarproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Order_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_);
+    }
+}
